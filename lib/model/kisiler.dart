0dc01a57091dc681270ea6ce551b70b863faee09
Taha Turan <tahaaturann16@gmail.com>
2022-06-28 05:06:02 +0300

    kisiler sinifi olusturuldu

---
+++ lib/model/kisiler.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: non_constant_identifier_names
+
+class Kisiler {
+  int kisi_id;
+  String kisi_ad;
+  int kisi_yas;
+  Kisiler(
+      {required this.kisi_id, required this.kisi_ad, required this.kisi_yas});
+}
